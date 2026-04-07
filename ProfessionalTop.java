c20672c064ab376d884a860acd74df6a674124c3
Anthony <123020590+AnthonyR02@users.noreply.github.com>
2026-04-06 21:09:56 -0400

    Add ProfessionalTop class implementing Top interface
    Implements the Top interface with wear and getDescription methods.

---
+++ ProfessionalTop.java
@@ -0,0 +1,21 @@
+
+public class ProfessionalTop implements Top {
+    
+    /**
+     * Display what wearing this top looks like
+     * Overrides the interface method
+     */
+    @Override
+    public void wear() {
+        System.out.println("👔 Wearing a crisp white dress shirt with a professional blazer");
+    }
+    
+    /**
+     * Get description of this top
+     * @return String describing this professional top
+     */
+    @Override
+    public String getDescription() {
+        return "Professional Blazer";
+    }
+}
