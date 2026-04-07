f3288ef1d839c9cffe1b54a1bb734d49ec6dd00d
Anthony <123020590+AnthonyR02@users.noreply.github.com>
2026-04-06 21:45:37 -0400

    Update and rename ProfessionalShoes.java to GarmentSimulator/ProfessionalShoes.java

--- GarmentSimulator/ProfessionalShoes.java
+++ GarmentSimulator/ProfessionalShoes.java
@@ -4,7 +4,7 @@ public class ProfessionalShoes implements Shoes {
 
     @Override
     public void wear() {
-        System.out.println("👞 Wearing polished black leather oxfords");
+        System.out.println("Wearing polished black leather oxfords");
     }
     
 
