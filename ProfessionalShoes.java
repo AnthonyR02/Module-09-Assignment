cffa8c14469371446ca0caf33e96aea2d86e8211
Anthony <123020590+AnthonyR02@users.noreply.github.com>
2026-04-06 21:11:49 -0400

    Implement ProfessionalShoes class with wear method

---
+++ ProfessionalShoes.java
@@ -0,0 +1,15 @@
+*/
+public class ProfessionalShoes implements Shoes {
+    
+
+    @Override
+    public void wear() {
+        System.out.println("👞 Wearing polished black leather oxfords");
+    }
+    
+
+    @Override
+    public String getDescription() {
+        return "Professional Oxfords";
+    }
+}
