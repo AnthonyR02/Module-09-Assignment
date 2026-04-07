f0f37dd6e290c252e0fda4d687c941103a370193
Anthony <123020590+AnthonyR02@users.noreply.github.com>
2026-04-06 21:18:30 -0400

    Implement PartyTop class with wear and description methods

---
+++ PartyTop.java
@@ -0,0 +1,12 @@
+
+public class PartyTop implements Top {
+    
+    @Override
+    public void wear() {
+        System.out.println("Wearing a glittery sequined top");
+      
+    @Override
+    public String getDescription() {
+        return "Party Sequined Top";
+    }
+}
